1b5b4bc9960e9eb5042782ef575dd87675a40b26
oakahn <mjpoak@gmail.com>
2019-11-27 10:51:55 +0700

    Initial Commit

---
+++ CleanSwiftV1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanSwiftV1
+//
+//  Created by Chayawat Suntornrak on 27/11/2562 BE.
+//  Copyright © 2562 Chayawat Suntornrak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
